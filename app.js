6b3725f38d8cc1d169d3f74751b0ef61ae7db2a4
Your Name In Single Quotes <your email here in single quotes>
2019-10-16 16:02:43 +0300

    3 done

--- app.js
+++ app.js
@@ -53,11 +53,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    var summing= sum(a,b);
+    var summing2= sum(summing[0],c)
+     var multiplying =multiply(a,b);
+     var multiplying2=multiply(multiplying[0],c)
+   return [summing2[0], multiplying2[0] ,a + " and " + b + " and " + c + " sum to " + summing2[0] +".", "The product of " + a + " and " + b + " and " + c + " is " + multiplying2[0] + "."];
+   
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
